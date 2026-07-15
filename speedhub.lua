225db55ca8f91a1ea61c86e3c0ad332a07abc459
goatdkai <goatdkai@gmail.com>
2026-07-15 11:48:50 +0800

    Create speedhub.lua

---
+++ speedhub.lua
@@ -0,0 +1,5 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CodedSai/GrowAGarden2/refs/heads/main/SpeedHubX"))()
+
+task.wait(0.5)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
